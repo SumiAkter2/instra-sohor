ec8f3397eceab9332cfea2283c4bb3bf2afce025
SumiAkter2 <akteralisha36@gmail.com>
2022-03-11 10:07:43 +0600

    same issue solve but like

--- js/app.js
+++ js/app.js
@@ -16,7 +16,8 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.plus(id); 
+    likedPostsId.push(id); 
+    // const likedPosts = posts.filter((post) => !likedPostsId.includes(post.id));
     showPosts(posts);
 };
 
