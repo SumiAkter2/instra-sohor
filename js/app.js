185fb3b5d2d33798f148d1ac334cc96d03b3d1b4
SumiAkter2 <akteralisha36@gmail.com>
2022-03-11 10:01:27 +0600

    solve report issue but not fully

--- js/app.js
+++ js/app.js
@@ -145,6 +145,7 @@ const showPosts = (posts) => {
 const displayLikedPosts = () => {
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
+      console.log(post);
         const div = createPost(post);
         document.getElementById( "liked" ).appendChild(div);
     });
@@ -152,10 +153,10 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+     reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
-    });
+     });
 };
 
 const loadPosts = async () =>{
